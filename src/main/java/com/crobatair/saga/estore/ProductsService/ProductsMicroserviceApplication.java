b5376a627ba0ab53646fbf073eaf8a30c2ede83e
Ale Vasquez <jose5m45@gmail.com>
2022-05-15 10:29:56 -0600

    feat: add discovery client configuration

--- src/main/java/com/crobatair/saga/estore/ProductsService/ProductsMicroserviceApplication.java
+++ src/main/java/com/crobatair/saga/estore/ProductsService/ProductsMicroserviceApplication.java
@@ -2,7 +2,9 @@ package com.crobatair.saga.estore.ProductsService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProductsMicroserviceApplication {
 
